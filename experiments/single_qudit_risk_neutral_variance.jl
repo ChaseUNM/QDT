a0ee85c2994a49839580645ce40363f88f10b8ae
Peter DelMastro <pdelmastro@vt.edu>
2026-04-09 14:23:51 -0400

    Revised single qudit risk neutral variance experiment to accept command line arguments

--- experiments/single_qudit_risk_neutral_variance.jl
+++ experiments/single_qudit_risk_neutral_variance.jl
@@ -1,257 +1,344 @@
-using LinearAlgebra, QuantumGateDesign, Random, Distributions, Printf
+using ArgParse, LinearAlgebra, Random, Distributions, Printf
+using QuantumGateDesign
 using Plots, Plots.Measures, LaTeXStrings, Plots.PlotMeasures
 using JLD2
 include("../src/digital_qudit.jl")
 include("../src/util.jl")
 
-############################################################################# 
-# PARAMETERS
-#############################################################################
 
-# Device 
-Ne = 2
-Ng = 2
-omega = 4.5
-omega_bias = 0.00
-omega_stdev = 0.001 * omega
-xi = 0.2 # no xi variance
-n_samples = 100
+###################################################################
+# SETUP ARGUMENT PARSER
+###################################################################
 
-# Gates
-gates = [PauliX, PauliY, PauliZ]
-n_gates = length(gates)
-T_gate = 15 * pi
+function parse_commandline()
+    s = ArgParseSettings()
 
-# Controls
-degree = 2
-n_splines = 8
-n_iters_opt = 100
-
-# Range of omega values over which to eval fidelity
-n_omega_eval = 100
-d_omega = 1.5 * omega_stdev  
-omega_min = omega - d_omega
-omega_max = omega + d_omega
-dt_eval_fidelity = 0.05
-
-# Number of times at which to evaluate the control functions
-# for plotting
-n_t_eval = 256
-
-
-n_trials = 2 # number of trials to perform
-
-seed = 1234 # rng seed
-
-############################################################################# 
-# SETUP
-#############################################################################
-
-# Set the seed for the RNG
-Random.seed!(seed)
-
-# Frequency parameter sampler
-omega_sampler = Normal(omega + omega_bias, omega_stdev)
-
-# Range of omegas over which to eval infidelities
-omegas = collect(LinRange(omega_min,omega_max,n_omega_eval))
-xi_eval = xi * ones(n_omega_eval) # No variance in self-kerr
-
-# Create a Qudit
-q = DigitalQudit(Ne, Ng)
-N = Ne + Ng
-add_param_samples(q, omega, xi) # Initializes the param hist
-
-# Create a control for each gate
-for i = 1:n_gates
-    # Control for this gate
-    qcontrol = FortranBSplineControl(degree, n_splines, T_gate)
-    add_control(q, gates[i], qcontrol)
-    # Infidelities for this gate
-    q.infidelity[gates[i]] = History(Vector{Float64})
-end
-
-
-############################################################################# 
-# RUN EXPERIMENT
-#############################################################################
-
-controls = zeros(n_trials, n_gates, 2, n_t_eval)
-infidelities = zeros(n_trials, n_gates, n_omega_eval)
-omega_samples = zeros(n_samples)
-
-for t = 1:n_trials
-
-    t_start = time()
-    @printf("\n==== TRIAL %d of %d ==== \n", t, n_trials)
-
-    # Parameter samples
-    omega_samples .= sort(rand(omega_sampler, n_samples))
-    xi_samples = xi * ones(n_samples)
-    update_param_samples(q, omega_samples, xi_samples)
-
-    for i = 1:n_gates
-        @printf("  Optimizing Gate %d of %d\n", i, n_gates)
-        # Randomize the controls
-        randomize_coeffs(q.controls[gates[i]])
-        # Optimize the gate
-        optimize_control(q, gates[i], 
-                            options=["max_iter" => n_iters_opt, "print_level" => 0, "limited_memory_max_history" => 250])
-        # Plot the control signal in the rotating frame
-        control_obj = q.controls[gates[i]].objs.values[end]
-        control_coeffs = q.controls[gates[i]].coeffs.values[end]
-        controls[t,i,1,:] = [
-                eval_p_derivative(control_obj, time, control_coeffs, 0)
-                for time in LinRange(0, T_gate, n_t_eval)
-            ]
-        controls[t,i,2,:] = [
-                eval_q_derivative(control_obj, time, control_coeffs, 0)
-                for time in LinRange(0, T_gate, n_t_eval)
-            ]
+    @add_arg_table s begin
+        ##### QUDIT ####
+        "--Ne"
+            help = "Number of essential levels"
+            arg_type = Int64
+            default = 2
+        "--Ng"
+            help = "Number of guard levels"
+            arg_type = Int64
+            default = 2
+        "--omega"
+            help = "Qudit true frequency"
+            arg_type = Float64
+            default = 4.5
+        "--xi"
+            help = "Qudit true self-kerr"
+            arg_type = Float64
+            default = 0.2
+        "--omega-stdev"
+            help = "Sampling stdev for qudit frequency, as a 
+                    proportion of omega"
+            arg_type = Float64
+            default = 1E-3
+        "--samples"
+            help = "Number of frequency samples per trial"
+            arg_type = Int
+            default = 10
+        ##### GATES ####
+        "--T-gate"
+            help = "Gate duration"
+            arg_type = Float64
+            default = 47.2
+        "--control-degree"
+            help = "Degree of the splines defining control signals"
+            arg_type = Int
+            default = 2
+        "--control-splines"
+            help = "Number of basis functions for the splines defining control signals"
+            arg_type = Int
+            default = 8
+        "--opt-iters"
+            help = "Maximum number of iterations for IPOPT control signal optimization"
+            arg_type = Int
+            default = 100
+        ##### MISC ####
+        "--trials"
+            help = "Number of independent trials to perform"
+            arg_type = Int
+            default = 2
+        "--seed"
+            help = "Seed for the global RNG"
+            arg_type = Int
+            default = -1 # Selected randomly
     end
 
-    # Set the parameter samples to be evenly spaced between
-    # omega_min and omega_max
+    return parse_args(s)
+end
 
-    update_param_samples(q, omegas, xi_eval)
-    q.omega_rot = mean(omega_samples) # Use the same rotatining frame frequency as the initial set of samples, as this is the frame in which the controls where optimized in
+###################################################################
+# PARSE ARGUMENTS
+###################################################################
 
-    # Measure fidelity over grid of omega values
+function main()
+
+    parsed_args = parse_commandline()
+
+    # Device 
+    Ne      = parsed_args["Ne"]
+    Ng      = parsed_args["Ng"]
+    omega   = parsed_args["omega"]
+    xi      = parsed_args["xi"] # no xi variance
+    omega_stdev = parsed_args["omega-stdev"] * omega
+    n_samples   = parsed_args["samples"]
+
+    # Gates
+    gates = [PauliX, PauliY, PauliZ]
+    n_gates = length(gates)
+    T_gate = parsed_args["T-gate"]
+
+    # Controls
+    degree    = parsed_args["control-degree"]
+    n_splines = parsed_args["control-splines"]
+    opt_iters = parsed_args["opt-iters"]
+
+    n_trials = parsed_args["trials"] # number of trials to perform
+
+    seed = parsed_args["seed"] # rng seed
+    if seed < 1
+        seed = Int(rand(UInt16))
+    end
+
+    # Range of omega values over which to eval fidelity
+    n_omega_eval = 150
+    d_omega = 2 * omega_stdev  
+    omega_min = omega - d_omega
+    omega_max = omega + d_omega
+    dt_eval_fidelity = 0.05
+
+    # Number of times at which to evaluate the control functions
+    # for plotting
+    n_t_eval = 256
+
+
+    ############################################################## 
+    # SETUP
+    ##############################################################
+
+    # Set the seed for the RNG
+    Random.seed!(seed)
+
+    # Frequency parameter sampler
+    omega_sampler = Normal(omega, omega_stdev)
+
+    # Range of omegas over which to eval infidelities
+    omegas_eval = collect(LinRange(omega_min,omega_max,n_omega_eval))
+    xi_eval = xi * ones(n_omega_eval) # No variance in self-kerr
+
+    # Create a Qudit
+    q = DigitalQudit(Ne, Ng)
+    N = Ne + Ng
+    add_param_samples(q, omega, xi) # Initializes the param hist
+
+    # Create a control for each gate
     for i = 1:n_gates
-        @printf("  Evaluating Gate %d of %d\n", i, n_gates)
-        U = unitary(gates[i], Ne+Ng) 
-        Psi = run_control(
-                q, 
-                q.controls[gates[i]], 
-                dt=dt_eval_fidelity
-              )
-        for k = 1:n_omega_eval
-            Psi_k = Psi[k,:,:]
-            foreach(normalize!, eachcol(Psi_k))
-            infidelities[t,i,k] = infidelity(Psi_k, U, Ne)
+        # Control for this gate
+        local qcontrol = FortranBSplineControl(degree, n_splines, T_gate)
+        add_control(q, gates[i], qcontrol)
+        # Infidelities for this gate
+        q.infidelity[gates[i]] = History(Vector{Float64})
+    end
+
+
+    ##############################################################
+    # RUN EXPERIMENT
+    ##############################################################
+
+    controls = zeros(n_trials, n_gates, 2, n_t_eval)
+    control_coeffs = zeros(n_trials, n_gates, degree*n_splines)
+    infidelities = zeros(n_trials, n_gates, n_omega_eval)
+    omega_samples = zeros(n_trials, n_samples)
+
+    for t = 1:n_trials
+
+        t_start = time()
+        @printf("\n==== TRIAL %d of %d ==== \n", t, n_trials)
+
+        # Parameter samples
+        omega_samples[t,:] .= sort(rand(omega_sampler, n_samples))
+        xi_samples = xi * ones(n_samples)
+        update_param_samples(q, omega_samples[t,:], xi_samples)
+
+        for i = 1:n_gates
+            @printf("  Optimizing Gate %d of %d\n", i, n_gates)
+            # Randomize the controls
+            randomize_coeffs(q.controls[gates[i]])
+            # Optimize the gate
+            optimize_control(q, gates[i], 
+                                options=["max_iter" => opt_iters, "print_level" => 0, "limited_memory_max_history" => 250])
+            # Plot the control signal in the rotating frame
+            local control_obj = q.controls[gates[i]].objs.values[end]
+            control_coeffs[t,i,:] = q.controls[gates[i]].coeffs.values[end]
+            controls[t,i,1,:] = [
+                    eval_p_derivative(
+                        control_obj, time, control_coeffs[t,i,:], 0
+                    )
+                    for time in LinRange(0, T_gate, n_t_eval)
+                ]
+            controls[t,i,2,:] = [
+                    eval_q_derivative(
+                        control_obj, time, control_coeffs[t,i,:], 0
+                    )
+                    for time in LinRange(0, T_gate, n_t_eval)
+                ]
         end
-    end
-    t_end = time()
-    @printf("Done. Runtime = %.0f s\n", t_end-t_start)
-end
 
-############################################################################# 
-# SAVE DATA TO FILE
-#############################################################################
+        # Set the parameter samples to be evenly spaced between
+        # omega_min and omega_max
+        update_param_samples(q, omegas_eval, xi_eval)
+        q.omega_rot = mean(omega_samples[t,:]) # Use the same rotatining frame frequency as the initial set of samples, as this is the frame in which the controls where optimized in
 
-output_dir = "./data/single_qudit_risk_neutral_variance"
-mkpath(output_dir)
-
-filename = @sprintf(
-    "N%d+%d_samples%d_trials%d_seed%d.jld2",
-    Ne, Ng, n_samples, n_trials, seed
-)
-full_path = joinpath(output_dir, filename)
-
-jldsave(full_path; omega, xi, omegas, controls, infidelities)
-
-
-############################################################################# 
-# PLOT INFIDELITIES
-#############################################################################
-
-c_min = 1e-5
-c_max = 1.0
-
-# Plot Infidelities vs parameter value
-subplots = Any[]
-gate_names = ["X", "Y", "Z"]
-for i = 1:n_gates
-    title_str = @sprintf(
-                    "Fidelity by Trial, \$%s\$ Gate", 
-                    gate_names[i]
+        # Measure fidelity over grid of omega values
+        for i = 1:n_gates
+            @printf("  Evaluating Gate %d of %d\n", i, n_gates)
+            U = unitary(gates[i], Ne+Ng) 
+            Psi = run_control(
+                    q, 
+                    q.controls[gates[i]], 
+                    dt=dt_eval_fidelity
                 )
-    p_i = plot(
-            title= LaTeXString(title_str), 
-            titlefontsize=16, 
-            xlabel=L"Frequency Error $(\omega-\omega_*)/\omega_*$", xlabelfontsize=14,
-            ylabel="Infidelity (Predicted)", ylabelfontsize=14, 
-            size=(600,500), dpi=512, 
-            ylim=(c_min,c_max), yscale=:log10,
-            right_margin = 10mm
-        )
-    for t = 1:n_trials
-        plot!(
-            (omegas .- omega) ./ omegas,
-            infidelities[t,i,:],
-            color=:blue, alpha=0.5, legend=false
-        )
+            for k = 1:n_omega_eval
+                Psi_k = Psi[k,:,:]
+                foreach(normalize!, eachcol(Psi_k))
+                infidelities[t,i,k] = infidelity(Psi_k, U, Ne)
+            end
+        end
+        t_end = time()
+        @printf("Done. Runtime = %.0f s\n", t_end-t_start)
     end
 
-    push!(subplots, p_i)
+    ##############################################################
+    # SAVE DATA TO FILE
+    ##############################################################
 
-end
+    output_dir = "./data/single_qudit_risk_neutral_variance"
+    mkpath(output_dir)
 
-f1 = plot(
-    subplots[1], subplots[2], subplots[3],
-    layout=(1,3), size=(1500,500), dpi=512, 
-    bottom_margin=10mm, top_margin=10mm
-)
+    filename = @sprintf(
+        "N%d+%d_stdev%.1e_samples%d_trials%d_seed%d.jld2",
+        Ne, Ng, omega_stdev, n_samples, n_trials, seed
+    )
+    full_path = joinpath(output_dir, filename)
 
-
-# Create figure output directory
-fig_dir = "./figures/single_qudit_risk_neutral_variance"
-mkpath(fig_dir)
-
-# Saving to file
-file_prefix = @sprintf(
-    "N%d+%d_samples%d_trials%d_seed%d",
-    Ne, Ng, n_samples, n_trials, seed
-)
-fig_path = joinpath(fig_dir, file_prefix * "_infidelities.svg")
-savefig(f1, fig_path)
+    jldsave(full_path; 
+            Ne, Ng, omega, xi,  
+            T_gate, degree, n_splines,  
+            omega_stdev, n_samples, omega_samples, seed, 
+            omegas_eval, 
+            controls, control_coeffs, infidelities)
 
 
+    ##############################################################
+    # PLOT INFIDELITIES
+    ##############################################################
 
-############################################################################# 
-# PLOT CONTROL SIGNALS
-#############################################################################
+    c_min = 1e-5
+    c_max = 1.0
 
-# Plot Infidelities vs parameter value
-control_plots = Any[]
-gate_names = ["X", "Y", "Z"]
-for i = 1:n_gates
-    title_str = @sprintf(
-                    "Controls by Trial, \$%s\$ Gate", 
-                    gate_names[i]
-                )
-    p_i = plot(
-            title= LaTeXString(title_str), 
-            titlefontsize=16, 
-            xlabel=L"Time $t$", xlabelfontsize=14,
-            ylabel="Control Amplitude, p(t) or q(t)", ylabelfontsize=14, 
-            size=(600,500), dpi=512, 
-            ylim=(-0.1,0.1),
-            right_margin = 10Plots.mm
-        )
-    for t = 1:n_trials
-        plot!(
-            LinRange(0, T_gate, n_t_eval),
-            controls[t,i,1,:],
-            color=:blue, alpha=0.5, legend=false
-        )
-        plot!(
-            LinRange(0, T_gate, n_t_eval),
-            controls[t,i,2,:],
-            color=:red, alpha=0.5, legend=false
-        )
+    # Plot Infidelities vs parameter value
+    subplots = Any[]
+    gate_names = ["X", "Y", "Z"]
+    for i = 1:n_gates
+        title_str = @sprintf(
+                        "Fidelity by Trial, \$%s\$ Gate", 
+                        gate_names[i]
+                    )
+        p_i = plot(
+                title= LaTeXString(title_str), 
+                titlefontsize=16, 
+                xlabel=L"Frequency Error $(\omega-\omega_*)/\omega_*$", xlabelfontsize=14,
+                ylabel="Infidelity (Predicted)", ylabelfontsize=14, 
+                size=(600,500), dpi=512, 
+                ylim=(c_min,c_max), yscale=:log10,
+                right_margin = 10mm
+            )
+        for t = 1:n_trials
+            plot!(
+                (omegas_eval .- omega) ./ omegas_eval,
+                infidelities[t,i,:],
+                color=:blue, alpha=0.5, legend=false
+            )
+        end
+
+        push!(subplots, p_i)
+
     end
 
-    push!(control_plots, p_i)
+    f1 = plot(
+        subplots[1], subplots[2], subplots[3],
+        layout=(1,3), size=(1500,500), dpi=512, 
+        bottom_margin=10mm, top_margin=10mm
+    )
 
-end
+
+    # Create figure output directory
+    fig_dir = "./figures/single_qudit_risk_neutral_variance"
+    mkpath(fig_dir)
+
+    # Saving to file
+    file_prefix = @sprintf(
+        "N%d+%d_stdev%.1e_samples%d_trials%d_seed%d",
+        Ne, Ng, omega_stdev, n_samples, n_trials, seed
+    )
+    fig_path = joinpath(fig_dir, file_prefix * "_infidelities.svg")
+    savefig(f1, fig_path)
 
 
-f2 = plot(
-    control_plots[1], control_plots[2], control_plots[3],
-    layout=(1,3), size=(1500,500), dpi=512,
-    bottom_margin=10mm, top_margin=10mm
-)
 
-fig_path = joinpath(fig_dir, file_prefix * "_controls.svg")
-savefig(f2, fig_path)
+    ##############################################################
+    # PLOT CONTROL SIGNALS
+    ##############################################################
+
+    # Plot Infidelities vs parameter value
+    control_plots = Any[]
+    gate_names = ["X", "Y", "Z"]
+    for i = 1:n_gates
+        title_str = @sprintf(
+                        "Controls by Trial, \$%s\$ Gate", 
+                        gate_names[i]
+                    )
+        p_i = plot(
+                title= LaTeXString(title_str), 
+                titlefontsize=16, 
+                xlabel=L"Time $t$", xlabelfontsize=14,
+                ylabel="Control Amplitude, p(t) or q(t)", ylabelfontsize=14, 
+                size=(600,500), dpi=512, 
+                ylim=(-0.1,0.1),
+                right_margin = 10Plots.mm
+            )
+        for t = 1:n_trials
+            plot!(
+                LinRange(0, T_gate, n_t_eval),
+                controls[t,i,1,:],
+                color=:blue, alpha=0.5, legend=false
+            )
+            plot!(
+                LinRange(0, T_gate, n_t_eval),
+                controls[t,i,2,:],
+                color=:red, alpha=0.5, legend=false
+            )
+        end
+
+        push!(control_plots, p_i)
+
+    end
+
+
+    f2 = plot(
+        control_plots[1], control_plots[2], control_plots[3],
+        layout=(1,3), size=(1500,500), dpi=512,
+        bottom_margin=10mm, top_margin=10mm
+    )
+
+    fig_path = joinpath(fig_dir, file_prefix * "_controls.svg")
+    savefig(f2, fig_path)
+
+end # end main()
+
+
+main()
